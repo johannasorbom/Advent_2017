45685fe4a0839744538f21e70731da607ec49929
johanna.sorbom <sorbom.johanna@gmail.com>
2018-02-24 17:19:10 +0100

    c

---
+++ Advent_2a.py
@@ -0,0 +1,28 @@
+A = [798,1976,1866,1862,559,1797,1129,747,85,1108,104]
+Min = min(A)
+Max = max (A)
+difference_A = Max - Min
+
+B = [2000,248,131,87,95]
+Min = min(B)
+Max = max (B)
+difference_B = Max - Min
+
+checksum = difference_A + difference_B
+print (checksum)
+
+#Test (should ecual 10)
+A = [1,2,3,5,6,7,8]
+Min = min(A)
+Max = max (A)
+difference_A = Max - Min
+
+B = [1,2,4]
+Min = min(B)
+Max = max (B)
+difference_B = Max - Min
+
+checksum = difference_A + difference_B
+print (checksum)
+
+
