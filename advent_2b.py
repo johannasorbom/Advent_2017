9af8533c46386d062aae52334f24bca798ec01d0
johanna.sorbom <sorbom.johanna@gmail.com>
2018-02-24 17:49:39 +0100

    2b

--- advent_2b.py
+++ advent_2b.py
@@ -4,13 +4,13 @@ d = []
 l = [line.split() for line in f]
 c = [list(map(int, x)) for x in l]
 
-for i in range (0,15):
-    for x in range (0,15):
-        for a in range (0,15):
+for i in range (16):
+    for x in range (16):
+        for a in range (16):
             if (c[i][x] % c[i][a]) == 0:
                 b = (c[i][x]/c[i][a])
                 d.append(b)
-     
+
 summa = sum(d)        
 print(summa)
 
